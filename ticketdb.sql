06ec8808f3ea9f3ef369a01c6ec996b894e4903f
lmlora <lmlora@uno.com.do>
2019-03-04 16:56:27 -0400

    agregando archivos con la estructura de la db

---
+++ ticketdb.sql
@@ -0,0 +1,176 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-03-2019 a las 20:49:23
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ticketdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `assigned_ticket`
+--
+
+CREATE TABLE `assigned_ticket` (
+  `id_record` int(11) NOT NULL,
+  `id_user_assigned` int(11) NOT NULL,
+  `id_ticket` int(11) NOT NULL,
+  `id_user_assigns_ticket` int(11) NOT NULL,
+  `assigned_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id_record` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
+  `id_user_requestor` int(11) NOT NULL,
+  `id_ticket_type` int(11) NOT NULL,
+  `request_date` datetime NOT NULL,
+  `id_ticket_status` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket_detail`
+--
+
+CREATE TABLE `ticket_detail` (
+  `id_record` int(11) NOT NULL,
+  `id_ticket` int(11) NOT NULL,
+  `comment` varchar(255) COLLATE utf8_bin NOT NULL,
+  `worked_time` float NOT NULL,
+  `id_ticket_status_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ticket_status`
+--
+
+CREATE TABLE `ticket_status` (
+  `id_record` int(11) NOT NULL,
+  `status_name` int(11) NOT NULL,
+  `description` varchar(50) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id_record` int(11) NOT NULL,
+  `first_name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `username` varchar(20) COLLATE utf8_bin NOT NULL,
+  `password` varchar(20) COLLATE utf8_bin NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id_record`, `first_name`, `last_name`, `username`, `password`, `active`) VALUES
+(1, 'Lissette', 'Lora', 'lmlora', '123456', 1),
+(2, 'Wilman', 'Hilario', 'whilario', '123456', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `assigned_ticket`
+--
+ALTER TABLE `assigned_ticket`
+  ADD PRIMARY KEY (`id_record`);
+
+--
+-- Indices de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id_record`);
+
+--
+-- Indices de la tabla `ticket_detail`
+--
+ALTER TABLE `ticket_detail`
+  ADD PRIMARY KEY (`id_record`);
+
+--
+-- Indices de la tabla `ticket_status`
+--
+ALTER TABLE `ticket_status`
+  ADD PRIMARY KEY (`id_record`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_record`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `assigned_ticket`
+--
+ALTER TABLE `assigned_ticket`
+  MODIFY `id_record` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id_record` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket_detail`
+--
+ALTER TABLE `ticket_detail`
+  MODIFY `id_record` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ticket_status`
+--
+ALTER TABLE `ticket_status`
+  MODIFY `id_record` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_record` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
